d0ce9f8b62052d4143617085b7182cc0b3df2d4a
Gabriel Bauer <bauer_gabriel@icloud.com>
2026-03-23 17:21:22 -0300

    Update queries.sql

--- data/sql/queries.sql
+++ data/sql/queries.sql
@@ -56,3 +56,19 @@ FROM supermarket
 GROUP BY "Customer type"
 ORDER BY total_gasto DESC;
 
+-- 📅 FATURAMENTO POR DIA DA SEMANA
+-- (0 = Domingo, 1 = Segunda, ..., 6 = Sábado)
+SELECT 
+    CASE CAST(strftime('%w', Date) AS INTEGER)
+        WHEN 0 THEN 'Domingo'
+        WHEN 1 THEN 'Segunda-feira'
+        WHEN 2 THEN 'Terça-feira'
+        WHEN 3 THEN 'Quarta-feira'
+        WHEN 4 THEN 'Quinta-feira'
+        WHEN 5 THEN 'Sexta-feira'
+        WHEN 6 THEN 'Sábado'
+    END AS dia_semana,
+    ROUND(SUM(Sales), 2) AS faturamento_total
+FROM supermarket
+GROUP BY dia_semana
+ORDER BY faturamento_total DESC;
